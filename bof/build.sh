b959d8a9926f31c04ffc54854ebdb92a9fce2519
BigB00st <botzer.2002@gmail.com>
2020-04-07 16:17:01 +0300

    modified build flags

--- bof/build.sh
+++ bof/build.sh
@@ -3,7 +3,5 @@ echo 0 | sudo tee /proc/sys/kernel/randomize_va_space
 gcc \
 -fno-stack-protector \
 -no-pie \
--z execstack \
--Wl,-z,norelro \
 -o bof bof.c
 echo 1 | sudo tee /proc/sys/kernel/randomize_va_space
--- canary/build.sh
+++ canary/build.sh
@@ -2,7 +2,5 @@
 echo 0 | sudo tee /proc/sys/kernel/randomize_va_space
 gcc \
 -no-pie \
--z execstack \
--Wl,-z,norelro \
 -o canary canary.c
 echo 1 | sudo tee /proc/sys/kernel/randomize_va_space
